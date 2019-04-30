ba9ad4aaaa86fb49d4285148a9cdaf68cf0edf82
HPNY <445700169@qq.com>
2019-04-30 11:24:17 +0800

    修改代码

---
+++ src/main/java/com/demo/entity/History.java
@@ -0,0 +1,49 @@
+package com.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer articleId;
+
+    public History() {
+    }
+
+    public History(Integer userId, Integer articleId) {
+        this.userId = userId;
+        this.articleId = articleId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+}
